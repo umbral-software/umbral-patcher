3ff35324d649f055cffc479cd3347cbe8911f4fa
Karen Webb <karen.webb@umbralsoftware.co.uk>
2025-07-08 05:56:14 +0100

    Format tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,10 +1,16 @@
-use crate::{parse_ips, Record, Result};
+use crate::{Record, Result, parse_ips};
 
 #[test]
 fn parse_single_normal() -> Result<()> {
     let ips = b"PATCH\x42\x69\x00\x00\x02\x00\x00EOF";
     let record = parse_ips(ips)?.into_iter().collect::<Vec<_>>();
-    assert_eq!(record, vec![Record::Normal { offset: 0x426900, data: Vec::from([0, 0]) }]);
+    assert_eq!(
+        record,
+        vec![Record::Normal {
+            offset: 0x426900,
+            data: Vec::from([0, 0])
+        }]
+    );
     Ok(())
 }
 
@@ -12,6 +18,13 @@ fn parse_single_normal() -> Result<()> {
 fn parse_single_rle() -> Result<()> {
     let ips = b"PATCH\x42\x69\x00\x00\x00\x04\x20\xFFEOF";
     let record = parse_ips(ips)?.into_iter().collect::<Vec<_>>();
-    assert_eq!(record, vec![Record::RLE { offset: 0x426900, size: 0x420, data:0xFF }]);
+    assert_eq!(
+        record,
+        vec![Record::RLE {
+            offset: 0x426900,
+            size: 0x420,
+            data: 0xFF
+        }]
+    );
     Ok(())
 }
