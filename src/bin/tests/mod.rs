d141dad18efadabbdfd7f296da6b444123165184
Karen Webb <karen.webb@umbralsoftware.co.uk>
2026-04-16 22:47:49 +0100

    Add test for auto-deduce filename with many '.'.

--- src/bin/tests/mod.rs
+++ src/bin/tests/mod.rs
@@ -11,3 +11,15 @@ fn generate_output_name() {
         Some(PathBuf::from(EXPECTED_OUTPUT))
     );
 }
+
+#[test]
+fn generate_output_name_many_dots() {
+    const INPUT: &str = "/foo/bar/baz.gba";
+    const IPS: &str = "/foo/ips/quux.v1.82.ips";
+    const EXPECTED_OUTPUT: &str = "/foo/bar/quux.v1.82.gba";
+
+    assert_eq!(
+        super::generate_output_name(Path::new(INPUT), Path::new(IPS)),
+        Some(PathBuf::from(EXPECTED_OUTPUT))
+    );
+}
